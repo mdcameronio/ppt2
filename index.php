815483f950522adb531b6db159465a38f206ec60
Mat Cameron <cameron.mathieu@student.greenriver.edu>
2022-01-11 17:17:19 -0500

    step 1 php array pratice

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+//mat casmeron
+//1/11/2022
+//url https://mcameron.greenriverdev.com/328/ppt2/index.php
+//pair program 2
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hello, world!</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="description" content="" />
+</head>
+<body>
+<?php
+echo "<p>1. PHP Array practice";
+
+
+?>
+
+</body>
+</html>
+
+
+
+
+
+<?php
+
+//mat casmeron
+//1/11/2022
+//url https://mcameron.greenriverdev.com/328/ppt2/index.php
+//pair program 2
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
